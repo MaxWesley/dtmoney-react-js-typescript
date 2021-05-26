65f3f5dd881d36c9813b3ccd76d30547c884ce00
MaxWesley <mx_wesley@hotmail.com>
2021-05-26 01:59:03 -0300

    feat: add methods and state of modal newTransact..

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,32 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
+
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header';
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global'
 
+Modal.setAppElement('#root')
+
 export function App() {
+  const [isNewTransactionModalIpen, setIsNewTransactionModalOpen] = useState(false)
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false)
+  }
   return (
     <>
-      <Header />
-      <GlobalStyle />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalIpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      <GlobalStyle />
     </>
   );
 }
